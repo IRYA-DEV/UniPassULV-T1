02d5587c691fbbfce7cf322dd1eb3fefafc08da4
IRYA-DEV <iyael117@gmail.com>
2024-11-06 11:24:59 -0600

    Correccion de un parametro en permission controller

--- src/controllers/permission.controller.js
+++ src/controllers/permission.controller.js
@@ -3,13 +3,46 @@ import sql from 'mssql';
 
 export const getPermissionsByUser = async (req, res) => {
     let pool;
+    const { page = 1, limit = 10 } = req.query; // Valores por defecto: página 1 y 10 elementos
+    const offset = (page - 1) * limit; // Calcular el desplazamiento (offset)
+
     try {
         pool = await getConnection();
         const result = await pool
             .request()
             .input('Id', sql.Int, req.params.Id)
-            .query('SELECT * FROM Permission JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin WHERE IdLogin = @Id');
-        res.json(result.recordset);
+            .input('Limit', sql.Int, parseInt(limit))
+            .input('Offset', sql.Int, parseInt(offset))
+            .query(`
+                SELECT * FROM Permission 
+                JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit 
+                JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin 
+                WHERE IdLogin = @Id
+                ORDER BY Permission.FechaSolicitada DESC
+                OFFSET @Offset ROWS FETCH NEXT @Limit ROWS ONLY
+            `);
+
+        // Consulta adicional para contar el total de permisos de usuario
+        const totalResult = await pool
+            .request()
+            .input('Id', sql.Int, req.params.Id)
+            .query(`
+                SELECT COUNT(*) as TotalPermissions FROM Permission 
+                WHERE IdUser = @Id
+            `);
+        const totalItems = totalResult.recordset[0].TotalPermissions;
+        const totalPages = Math.ceil(totalItems / limit);
+
+        // Responder con los datos y los metadatos de paginación
+        res.json({
+            data: result.recordset,
+            pagination: {
+                totalItems,
+                totalPages,
+                currentPage: parseInt(page),
+                limit: parseInt(limit)
+            }
+        });
     } catch (error) {
         console.error('Error en el servidor:', error);
         res.status(500).send(error.message);
@@ -23,6 +56,7 @@ export const getPermissionsByUser = async (req, res) => {
         }
     }
 };
+
 
 export const getPermission = async (req, res) => {
     let pool;
@@ -64,13 +98,26 @@ export const createPermission = async (req, res) => {
             .query('SELECT 1 FROM dbo.LoginUniPass WHERE IdLogin = @IdUser');
 
         if (userResult.recordset.length > 0) {
+            // Resta 6 horas a cada fecha
+            const fechaSolicitada = new Date(req.body.FechaSolicitada);
+            fechaSolicitada.setHours(fechaSolicitada.getHours() - 6);
+            const fechaSalida = new Date(req.body.FechaSalida);
+            fechaSalida.setHours(fechaSalida.getHours() - 6);
+            const fechaRegreso = new Date(req.body.FechaRegreso);
+            fechaRegreso.setHours(fechaRegreso.getHours() - 6);
+
+            // Convertir fechas a UTC
+            const fechaSolicitadaUTC = fechaSolicitada.toISOString();
+            const fechaSalidaUTC = fechaSalida.toISOString();
+            const fechaRegresoUTC = fechaRegreso.toISOString();
+
             // Inserta en la tabla Permission
             const result = await pool
                 .request()
-                .input('FechaSolicitada', sql.DateTime, req.body.FechaSolicitada)
+                .input('FechaSolicitada', sql.DateTime, fechaSolicitadaUTC)
                 .input('StatusPermission', sql.VarChar, req.body.StatusPermission)
-                .input('FechaSalida', sql.DateTime, req.body.FechaSalida)
-                .input('FechaRegreso', sql.DateTime, req.body.FechaRegreso)
+                .input('FechaSalida', sql.DateTime, fechaSalidaUTC)
+                .input('FechaRegreso', sql.DateTime, fechaRegresoUTC)
                 .input('Motivo', sql.VarChar, req.body.Motivo)
                 .input('IdUser', sql.Int, req.body.IdUser)
                 .input('IdTipoSalida', sql.Int, req.body.IdTipoSalida)
@@ -80,10 +127,10 @@ export const createPermission = async (req, res) => {
             console.log(result);
             res.json({
                 Id: result.recordset[0].IdPermission,
-                FechaSolicitada: req.body.FechaSolicitada,
+                FechaSolicitada: fechaSolicitadaUTC,
                 StatusPermission: req.body.StatusPermission,
-                FechaSalida: req.body.FechaSalida,
-                FechaRegreso: req.body.FechaRegreso,
+                FechaSalida: fechaSalidaUTC,
+                FechaRegreso: fechaRegresoUTC,
                 Motivo: req.body.Motivo,
                 MedioSalida: req.body.MedioSalida,
                 IdUser: req.body.IdUser,
@@ -105,6 +152,8 @@ export const createPermission = async (req, res) => {
         }
     }
 };
+
+
 
 export const cancelPermission = async (req, res) => {
     let pool;
@@ -185,7 +234,7 @@ export const getPermissionForAutorizacion = async (req, res) => {
                 console.error('Error al cerrar la conexión a la base de datos:', closeError);
             }
         }
-    }
+    } 
 };
 
 export const getPermissionForAutorizacionPrece = async (req, res) => {
@@ -194,7 +243,7 @@ export const getPermissionForAutorizacionPrece = async (req, res) => {
         pool = await getConnection();
         const result = await pool.request()
             .input("Id", sql.Int, req.params.Id)
-            .query("SELECT Permission.*, TypeExit.*, LoginUniPass.* FROM Permission INNER JOIN Authorize ON Permission.IdPermission = Authorize.IdPermission JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin WHERE Authorize.IdEmpleado = 41 AND Permission.IdPermission IN (SELECT A1.IdPermission FROM Authorize A1 GROUP BY A1.IdPermission HAVING COUNT(A1.IdAuthorize) = 1) UNION SELECT Permission.*, TypeExit.*, LoginUniPass.* FROM Permission INNER JOIN Authorize ON Permission.IdPermission = Authorize.IdPermission JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin WHERE Authorize.IdEmpleado = @Id AND Permission.IdPermission IN (SELECT A1.IdPermission FROM Authorize A1 WHERE A1.StatusAuthorize = 'Aprobada'AND A1.IdAuthorize = (SELECT TOP 1 A2.IdAuthorize FROM Authorize A2 WHERE A2.IdPermission = A1.IdPermission ORDER BY A2.IdAuthorize));")
+            .query("SELECT Permission.*, TypeExit.*, LoginUniPass.* FROM Permission INNER JOIN Authorize ON Permission.IdPermission = Authorize.IdPermission JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin WHERE Authorize.IdEmpleado = @Id AND Permission.IdPermission IN (SELECT A1.IdPermission FROM Authorize A1 GROUP BY A1.IdPermission HAVING COUNT(A1.IdAuthorize) = 1) UNION SELECT Permission.*, TypeExit.*, LoginUniPass.* FROM Permission INNER JOIN Authorize ON Permission.IdPermission = Authorize.IdPermission JOIN TypeExit ON Permission.IdTipoSalida = TypeExit.IdTypeExit JOIN LoginUniPass ON Permission.IdUser = LoginUniPass.IdLogin WHERE Authorize.IdEmpleado = @Id AND Permission.IdPermission IN (SELECT A1.IdPermission FROM Authorize A1 WHERE A1.StatusAuthorize = 'Aprobada'AND A1.IdAuthorize = (SELECT TOP 1 A2.IdAuthorize FROM Authorize A2 WHERE A2.IdPermission = A1.IdPermission ORDER BY A2.IdAuthorize));")
 
         if (result.rowsAffected[0] === 0) {
             return res.status(404).json({ message: "Dato no encontrado" });
